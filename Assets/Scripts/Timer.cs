4815656bf60cff94473dba74f305ca0a981b6a80
Felix <felixgrundtvig@gmail.com>
2026-04-11 23:46:06 +0200

    some timer code yayyy (not at all finished(i cant concentrate))

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,11 +1,28 @@
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    
+    public float timeRemaining = 100;
+    public TMP_Text timeDisplayed;
 
     void Update()
     {
-        
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            CountdownFinished();
+        }
     }
+
+    void CountdownFinished()
+    {
+
+    }
+
+    
 }
