1840c8ee14c83817aeffe1549b292e4c2c1bc817
seb.nor.san <sejeseb99@gmail.com>
2026-04-11 15:21:39 +0200

    Added evidencemanager for final cutscene

---
+++ Assets/Scripts/EvidenceManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvidenceManager : MonoBehaviour
+{
+    public static EvidenceManager instance;
+
+    [SerializeField] private List<Evidence> evidenceOrder;
+
+    [Space(5)]
+
+    //For example bloodied hands / clothes, or the clothes being in the drier
+    [SerializeField] private Evidence specialEvidenceActive = null;
+
+    [Space(5)]
+
+    [SerializeField] private EvidenceEvents[] evidenceEvents;
+
+    [Serializable]
+    public class EvidenceEvents
+    {
+        public Evidence evidenceBase;
+        public int eventId;
+    }
+	private void Start()
+	{
+        var allEvidence = FindObjectsByType<Evidence>();
+
+        foreach (var evidence in allEvidence)
+        {
+            if (!evidenceOrder.Contains(evidence))
+                Debug.LogWarning("Evidence called: " + evidence.gameObject.name + " Has not been put in order!");
+        }
+	}
+	public void SetSpecialEvidenceEvent(int id)
+    {
+        Evidence eToUse = null;
+
+        if (id != -1)
+            foreach (var evidenceEvent in evidenceEvents)
+            {
+                if (evidenceEvent.eventId == id)
+                    eToUse = evidenceEvent.evidenceBase;
+            }
+
+        specialEvidenceActive = eToUse;
+    }
+    public void ResetSpecialEvidenceEvent()
+    {
+        SetSpecialEvidenceEvent(-1);
+    }
+    public void RemoveEvidence(Evidence evidence)
+    {
+        if (evidenceOrder.Contains(evidence))
+            evidenceOrder.Remove(evidence);
+    }
+}
