7e4abcf4b4038ec2989da9a3e718fdb377464086
seb.nor.san <sejeseb99@gmail.com>
2026-04-12 13:59:06 +0200

    afaf

--- Assets/Scripts/IntroAnimation.cs
+++ Assets/Scripts/IntroAnimation.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class IntroAnimation : MonoBehaviour
 {
-    public void AE_FadeToBlack()
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			ScreenSummoner.SummonScreen(Color.black, .5f, true);
+			Invoke(nameof(Change), 1f);
+		}
+	}
+	public void AE_FadeToBlack()
     {
         ScreenSummoner.SummonScreen(Color.black, 1f, true);
     }
+	private void Change()
+	{
+		GetComponent<SceneSwitcher>().ChangeScene();
+	}
 }
